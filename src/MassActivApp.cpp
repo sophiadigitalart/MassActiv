fb2bb4ba77b30d31ce5c7c8f8aa7d54159b5c826
Bruce LANE <bruce@batchass.fr>
2019-05-05 19:12:48 +0200

    lpm2019

--- src/MassActivApp.cpp
+++ src/MassActivApp.cpp
@@ -65,6 +65,13 @@ private:
 	void							positionRenderWindow();
 	bool							mFadeInDelay;
 	SpoutOut 						mSpoutOut;
+	int								mode;
+	//! fbos
+	void							renderToFbo();
+	gl::FboRef						mFbo;
+	//! shaders
+	gl::GlslProgRef					mGlsl;
+	bool							mUseShader;
 };
 
 
@@ -86,8 +93,18 @@ MassActivApp::MassActivApp()
 	// windows
 	mIsShutDown = false;
 	mRenderWindowTimer = 0.0f;
+	mode = 0;
 	//timeline().apply(&mRenderWindowTimer, 1.0f, 2.0f).finishFn([&] { positionRenderWindow(); });
+		// fbo
+	gl::Fbo::Format format;
+	//format.setSamples( 4 ); // uncomment this to enable 4x antialiasing
+	mFbo = gl::Fbo::create(mSDASettings->mRenderWidth, mSDASettings->mRenderHeight, format.depthTexture());
+	// shader
+	mUseShader = true;
+	mGlsl = gl::GlslProg::create(gl::GlslProg::Format().vertex(loadAsset("passthrough.vs")).fragment(loadAsset("post.glsl")));
 
+	gl::enableDepthRead();
+	gl::enableDepthWrite();
 }
 void MassActivApp::resizeWindow()
 {
@@ -113,11 +130,7 @@ void MassActivApp::fileDrop(FileDropEvent event)
 {
 	mSDASession->fileDrop(event);
 }
-void MassActivApp::update()
-{
-	mSDASession->setFloatUniformValueByIndex(mSDASettings->IFPS, getAverageFps());
-	mSDASession->update();
-}
+
 void MassActivApp::cleanup()
 {
 	if (!mIsShutDown)
@@ -140,7 +153,7 @@ void MassActivApp::mouseDown(MouseEvent event)
 {
 	if (!mSDASession->handleMouseDown(event)) {
 		// let your application perform its mouseDown handling here
-		if (event.isRightDown()) { 
+		if (event.isRightDown()) {
 		}
 	}
 }
@@ -148,7 +161,7 @@ void MassActivApp::mouseDrag(MouseEvent event)
 {
 	if (!mSDASession->handleMouseDrag(event)) {
 		// let your application perform its mouseDrag handling here
-	}	
+	}
 }
 void MassActivApp::mouseUp(MouseEvent event)
 {
@@ -161,9 +174,8 @@ void MassActivApp::keyDown(KeyEvent event)
 {
 	if (!mSDASession->handleKeyDown(event)) {
 		switch (event.getCode()) {
-		case KeyEvent::KEY_ESCAPE:
-			// quit the application
-			quit();
+		case KeyEvent::KEY_m:
+			mUseShader = !mUseShader;
 			break;
 		case KeyEvent::KEY_h:
 			// mouse cursor and ui visibility
@@ -178,7 +190,55 @@ void MassActivApp::keyUp(KeyEvent event)
 	if (!mSDASession->handleKeyUp(event)) {
 	}
 }
+// Render into the FBO
+void MassActivApp::renderToFbo()
+{
 
+	// this will restore the old framebuffer binding when we leave this function
+	// on non-OpenGL ES platforms, you can just call mFbo->unbindFramebuffer() at the end of the function
+	// but this will restore the "screen" FBO on OpenGL ES, and does the right thing on both platforms
+	gl::ScopedFramebuffer fbScp(mFbo);
+	// clear out the FBO with black
+	gl::clear(Color::black());
+
+	// setup the viewport to match the dimensions of the FBO
+	gl::ScopedViewport scpVp(ivec2(0), mFbo->getSize());
+
+	// render
+
+	// texture binding must be before ScopedGlslProg
+	mSDASession->getMixetteTexture()->bind(0);
+
+	/*mSDASession->getFboTexture(5)->bind(0);
+	mSDASession->getFboTexture(6)->bind(1);
+	mSDASession->getFboTexture(7)->bind(2); */
+
+	gl::ScopedGlslProg prog(mGlsl);
+
+	mGlsl->uniform("iGlobalTime", (float)getElapsedSeconds());
+	mGlsl->uniform("iResolution", vec3(mSDASettings->mRenderWidth, mSDASettings->mRenderHeight, 1.0));
+	mGlsl->uniform("iChannel0", 0); // texture 0
+	mGlsl->uniform("iChannel1", 1);
+	//mGlsl->uniform("iChannel2", 2);
+	/*mGlsl->uniform("iExposure", 1.0f);
+mGlsl->uniform("iSobel", 1.0f);
+	mGlsl->uniform("iChromatic", 1.0f);		*/
+	mGlsl->uniform("iExposure", mSDASession->getFloatUniformValueByName("iExposure"));
+	mGlsl->uniform("iSobel", mSDASession->getFloatUniformValueByName("iSobel"));
+	mGlsl->uniform("iChromatic", mSDASession->getFloatUniformValueByName("iChromatic"));
+
+	gl::drawSolidRect(getWindowBounds());
+
+}
+void MassActivApp::update()
+{
+	mSDASession->setFloatUniformValueByIndex(mSDASettings->IFPS, getAverageFps());
+	mSDASession->update();
+	// render into our FBO
+	if (mUseShader) {
+		renderToFbo();
+	}
+}
 void MassActivApp::draw()
 {
 	gl::clear(Color::black());
@@ -189,15 +249,54 @@ void MassActivApp::draw()
 			timeline().apply(&mSDASettings->iAlpha, 0.0f, 1.0f, 1.5f, EaseInCubic());
 		}
 	}
+	mode = mSDASession->getMode();
+	CI_LOG_V("mode " + toString(mode));
+	gl::setMatricesWindow(toPixels(getWindowSize()), true);
+	//gl::setMatricesWindow(mSDASettings->mRenderWidth, mSDASettings->mRenderHeight, true);
+	if (mUseShader) {
+		gl::draw(mFbo->getColorTexture(), getWindowBounds());
+	}
+	else {
 
-	//gl::setMatricesWindow(toPixels(getWindowSize()),false);
-	gl::setMatricesWindow(mSDASettings->mRenderWidth, mSDASettings->mRenderHeight, false);
-	gl::draw(mSDASession->getMixTexture(), getWindowBounds());
+		switch (mode)
+		{
+		case 1:
+			gl::draw(mSDASession->getMixTexture(), getWindowBounds());
+			break;
+		case 2:
+			gl::draw(mSDASession->getRenderTexture(), getWindowBounds());
+			break;
+		case 3:
+			gl::draw(mSDASession->getHydraTexture(), getWindowBounds());
+			break;
+		case 4:
+			gl::draw(mSDASession->getFboTexture(0), getWindowBounds());
+			break;
+		case 5:
+			gl::draw(mSDASession->getFboTexture(1), getWindowBounds());
+			break;
+		case 6:
+			gl::draw(mSDASession->getFboTexture(2), getWindowBounds());
+			break;
+		case 7:
+			gl::draw(mSDASession->getFboTexture(3), getWindowBounds());
+			break;
+		case 8:
+			gl::draw(mSDASession->getMixetteTexture(), getWindowBounds());
+			break;
+		default:
+			gl::draw(mSDASession->getMixetteTexture(), getWindowBounds());
+
+			break;
+		}
+	}
 
 	// Spout Send
 	mSpoutOut.sendViewport();
-	mSDAUI->Run("UI", (int)getAverageFps());
-	if (mSDAUI->isReady()) {
+	if (mSDASettings->mCursorVisible) {
+		mSDAUI->Run("UI", (int)getAverageFps());
+		if (mSDAUI->isReady()) {
+		}
 	}
 	getWindow()->setTitle(mSDASettings->sFps + " MassActiv");
 }
